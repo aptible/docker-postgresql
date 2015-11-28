dbddbfebe9e66131c148df166d8181cdde410cb3
Frank Macreery <frank@macreery.com>
2015-11-28 17:38:40 -0500

    Replication support for 9.4, with integration tests
    This PR introduces a new API for administering replication via a read-write
    "leader" and read-only "follower." Specifically, the standardized workflow
    for promoting an existing standalone database to a leader, and creating a
    new follower database, is:
    
    1. Activate leader: Run a container with `--activate-leader LEADER_URL`,
       also mounting the data volumes of the leader container. This allows for
       enabling replication either remotely (by the LEADER_URL), or by directly
       accessing the leader's filesystem. This operation should print all
       necessary configuration variables to be loaded in (via ENV) to initialize
       the follower in the next step.
    2. Initialize follower: Run a container with `--initialize-follower`, in
       the same way you'd run `--initialize` on a standalone database. Mount a
       new set of data volumes for this container, and include all ENV variables
       printed by `--activate-leader` in the previous step.
    3. Start follower: Run a container with no arguments, mounting the data
       volumes of the follower created in the previous step.
    
    On PostgreSQL, these operations are implemented as follows:
    
    1. Activate leader: Create a new replication-only user, and print its
       connection URL as REPLICATION_URL.
    2. Initialize follower: Run pg_basebackup and create a recovery.conf file.
    3. Start follower: No special implementation. A PostgreSQL database "knows"
       whether it is a leader or follower.
    
    See aptible/support#182

--- 9.4/run-database.sh
+++ 9.4/run-database.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+. /usr/bin/utilities.sh
+
 command="/usr/lib/postgresql/$PG_VERSION/bin/postgres -D "$DATA_DIRECTORY" -c config_file=/etc/postgresql/$PG_VERSION/main/postgresql.conf"
 
-sed "s:DATA_DIRECTORY:${DATA_DIRECTORY}:g" /etc/postgresql/${PG_VERSION}/main/postgresql.conf.erb > /etc/postgresql/${PG_VERSION}/main/postgresql.conf
+if [ ! -f /etc/postgresql/${PG_VERSION}/main/postgresql.conf ]; then
+  erb /etc/postgresql/${PG_VERSION}/main/postgresql.conf.erb > /etc/postgresql/${PG_VERSION}/main/postgresql.conf
+fi
 
 if [[ "$1" == "--initialize" ]]; then
   chown -R postgres:postgres "$DATA_DIRECTORY"
@@ -15,18 +19,33 @@ if [[ "$1" == "--initialize" ]]; then
     /etc/init.d/postgresql stop
 COMMANDS
 
+elif [[ "$1" == "--initialize-follower" ]]; then
+  chown -R postgres:postgres "$DATA_DIRECTORY"
+  chmod 0700 "$DATA_DIRECTORY"
+  su postgres <<COMMANDS
+    pg_basebackup -D $DATA_DIRECTORY -R -d $REPLICATION_URL
+COMMANDS
+
+elif [[ "$1" == "--activate-leader" ]]; then
+  [ -z "$2" ] && echo "docker run -it aptible/postgresql --activate-leader postgresql://..." && exit 1
+  USERNAME=${REPLICATION_USERNAME:-replicator}
+  PASSPHRASE=${REPLICATION_PASSPHRASE:-$(random_passphrase)}
+  psql "$2" --command "CREATE USER $USERNAME REPLICATION LOGIN ENCRYPTED PASSWORD '$PASSPHRASE'" > /dev/null || exit 1
+  parse_url "$2"
+  echo "REPLICATION_URL=$protocol$USERNAME:$PASSPHRASE@$host_and_port/$database"
+
 elif [[ "$1" == "--client" ]]; then
-  [ -z "$2" ] && echo "docker run -it aptible/postgresql --client postgresql://..." && exit
+  [ -z "$2" ] && echo "docker run -it aptible/postgresql --client postgresql://..." && exit 1
   psql "$2"
 
 elif [[ "$1" == "--dump" ]]; then
-  [ -z "$2" ] && echo "docker run aptible/postgresql --dump postgresql://... > dump.psql" && exit
+  [ -z "$2" ] && echo "docker run aptible/postgresql --dump postgresql://... > dump.psql" && exit 1
   # If the file /dump-output exists, write output there. Otherwise, use stdout.
   [ -e /dump-output ] && exec 3>/dump-output || exec 3>&1
   pg_dump "$2" >&3
 
 elif [[ "$1" == "--restore" ]]; then
-  [ -z "$2" ] && echo "docker run -i aptible/postgresql --restore postgresql://... < dump.psql" && exit
+  [ -z "$2" ] && echo "docker run -i aptible/postgresql --restore postgresql://... < dump.psql" && exit 1
   # If the file /restore-input exists, read input there. Otherwise, use stdin.
   [ -e /restore-input ] && exec 3</restore-input || exec 3<&0
   psql "$2" <&3
