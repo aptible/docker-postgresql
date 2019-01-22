a04312957300f99c16d975633c52d731f3e61489
Alex Kubacki <alex@aptible.com>
2019-01-22 16:28:29 -0500

    Add support for Postgres 11.

--- 11/config.mk
+++ 11/config.mk
@@ -1,5 +1,5 @@
 export DEBIAN_VERSION = stretch
-export POSTGRES_VERSION = 9.6
-export POSTGIS_VERSION = 2.3
+export POSTGRES_VERSION = 11
+export POSTGIS_VERSION = 2.5
 export AUTH_METHOD = peer
 export PRELOAD_LIB = pg_stat_statements
