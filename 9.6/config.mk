d7d3b0e6663cf2bd590e98b744149b1263ee8fb9
Alex Kubacki <usernotfound@gmail.com>
2020-08-24 15:58:51 -0600

    Install some newer versions to facilitate upgrades.

--- 9.6/config.mk
+++ 9.6/config.mk
@@ -1,5 +1,5 @@
 export DEBIAN_VERSION = jessie
 export POSTGRES_VERSION = 9.6
-export POSTGIS_VERSION = 2.3
+export POSTGIS_VERSION = "2.3 2.4 2.5"
 export AUTH_METHOD = peer
 export PRELOAD_LIB = "pg_stat_statements,pglogical"
