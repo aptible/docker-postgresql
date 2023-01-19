2c059245ee54e27a5231260ae4cc893fd3d77e1c
John Turner <john.turner@aptible.com>
2023-01-19 11:54:21 -0500

    Adding changes for apt repos

--- 12/install-extras.sh
+++ 12/install-extras.sh
@@ -2,8 +2,12 @@
 set -o errexit
 set -o nounset
 set -x
+
+# Install apt-transport-https
+apt-install apt-transport-https
+
 # We'll need the pglogical repo...
-echo "deb [arch=amd64] http://packages.2ndquadrant.com/pglogical/apt/ stretch-2ndquadrant main" >> /etc/apt/sources.list
+echo "deb [arch=amd64] https://dl.2ndquadrant.com/default/release/apt stretch-2ndquadrant main" >> /etc/apt/sources.list
 
 # ...and its key
 apt-key add /tmp/GPGkeys/pglogical.key
