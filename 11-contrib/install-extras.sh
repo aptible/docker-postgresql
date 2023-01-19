c71f3941d3d7e4300d29c0b8b364ec4595b50d66
John Turner <john.turner@aptible.com>
2023-01-19 13:31:47 -0500

    Updating Order of Operations for build.

--- 11-contrib/install-extras.sh
+++ 11-contrib/install-extras.sh
@@ -3,9 +3,6 @@ set -o errexit
 set -o nounset
 set -x
 
-# Install apt-transport-https
-apt-install apt-transport-https
-
 # We'll need the pglogical repo...
 echo "deb [arch=amd64] https://dl.2ndquadrant.com/default/release/apt stretch-2ndquadrant main" >> /etc/apt/sources.list
 
